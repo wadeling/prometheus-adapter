2cced2608a10ff4c2aa5245cd3f97e1734ecf4fb
wadeling <wadeling@gmail.com>
2020-03-20 17:44:36 +0800

    add write handle

--- cmd/adapter/main.go
+++ cmd/adapter/main.go
@@ -2,8 +2,110 @@ package main
 
 import (
 	"fmt"
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+
+	//"github.com/prometheus/client_golang/prometheus"
+	//"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/wadeling/prometheus-adapter/pkg/logger"
+	"io/ioutil"
+	"net/http"
+	"os"
+	//"sync"
 )
+
+type prometheusAdapter struct {
+	listenPort string
+}
+
+var log = logger.NewLogger(true)
+
+func defaultAdapter() *prometheusAdapter{
+	return &prometheusAdapter{
+		listenPort: "9100",
+	}
+}
+
+func GetCmd() *cobra.Command {
+	adapter := defaultAdapter()
+
+	cmd := &cobra.Command{
+		Use:   "prometheus-adapter",
+		Short: "prometheus-adapter",
+		Run: func(cmd *cobra.Command, args []string) {
+			adapter.run()
+		},
+	}
+
+	f := cmd.PersistentFlags()
+	f.StringVarP(&adapter.listenPort, "port", "p", adapter.listenPort, "TCP port to use for adapter")
+
+	return cmd
+}
+
+func protoToSamples(req *prompb.WriteRequest) model.Samples {
+	var samples model.Samples
+	for _, ts := range req.Timeseries {
+		metric := make(model.Metric, len(ts.Labels))
+		for _, l := range ts.Labels {
+			metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+		}
+
+		for _, s := range ts.Samples {
+			samples = append(samples, &model.Sample{
+				Metric:    metric,
+				Value:     model.SampleValue(s.Value),
+				Timestamp: model.Time(s.Timestamp),
+			})
+		}
+	}
+	return samples
+}
+
+func (m *prometheusAdapter) run() error {
+	log.Info("run")
+	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
+		compressed, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Error("read error",zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		reqBuf, err := snappy.Decode(nil, compressed)
+		if err != nil {
+			log.Error("Decode err",zap.Error(err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var req prompb.WriteRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			log.Error("Unmarshal error",zap.Error(err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		samples := protoToSamples(&req)
+		log.Info("Recieve sample count",zap.Any("sample-count",samples.Len()))
+	})
+
+	//todo
+	addr := "localhost:9100"
+
+	return http.ListenAndServe(addr, nil)
+}
 
 func main() {
 	fmt.Println("hello adapter")
+	log.Info("hello adapter")
+
+	cmd := GetCmd()
+	if err := cmd.Execute(); err != nil {
+		os.Exit(-1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/wadeling/prometheus-adapter
 
 go 1.13
+
+require (
+	github.com/gogo/protobuf v1.2.1
+	github.com/golang/snappy v0.0.1
+	github.com/prometheus/client_golang v0.9.3
+	github.com/prometheus/common v0.4.0
+	github.com/prometheus/prometheus v2.5.0+incompatible
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.6
+	go.uber.org/zap v1.10.0
+)
